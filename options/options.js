ea1581780ffce997f8d5be6d5dfeb5285d7a04ed
Crowdbotics <team@crowdbotics.com>
2023-06-15 04:45:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-scene-41500",
+  url: "https://falling-scene-41500.botics.co",
+  api: "https://falling-scene-41500.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
